154723ee4d6b40ace34bf22419cad02b42a3c3f9
yinuoma112-dot <yinuoma112@gmail.com>
2025-11-28 10:18:58 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using WindowsAISample.ViewModels;
+using WindowsAISample.Pages;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.Runtime.InteropServices;
+using System;
+using Microsoft.UI.Xaml.Media;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace WindowsAISample;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        rootFrame.DataContext = new CopilotRootViewModel();
+        rootFrame.Navigate(typeof(TextRecognizerPage));
+    }
+
+    internal async void ShowException(Exception? ex, string? optionalMessage = null)
+    {
+        var msg = optionalMessage ?? ex switch {
+            COMException
+                when ex.Message.Contains("the rpc server is unavailable", StringComparison.CurrentCultureIgnoreCase) =>
+                    "The WCL is in an unstable state.\nRebooting the machine will restart the WCL.",
+            _ => $"Error:\n{ex?.Message ?? string.Empty}{(optionalMessage != null ? "\n" + optionalMessage : string.Empty)}"
+        };
+
+        var errorText = new TextBlock {
+            TextWrapping = TextWrapping.Wrap,
+            Text = msg,
+            IsTextSelectionEnabled = true,
+        };
+
+        ContentDialog exceptionDialog = new() {
+            Title = "Something went wrong",
+            Content = errorText,
+            PrimaryButtonText = "Copy error details",
+            SecondaryButtonText = "Reload",
+            XamlRoot = Content.XamlRoot,
+            CloseButtonText = "Close",
+            PrimaryButtonStyle = (Style)App.Current.Resources["AccentButtonStyle"],
+        };
+
+        var result = await exceptionDialog.ShowAsync();
+
+        if (result == ContentDialogResult.Primary)
+        {
+            CopyExceptionToClipboard(ex, optionalMessage);
+        }
+        else if (result == ContentDialogResult.Secondary)
+        {
+            rootFrame.Navigate(typeof(TranslatorPage));
+        }
+    }
+
+    public static void CopyExceptionToClipboard(Exception? ex, string? optionalMessage)
+    {
+        string exceptionDetails = string.IsNullOrWhiteSpace(optionalMessage) ? string.Empty : optionalMessage + "\n";
+
+        if (ex != null)
+        {
+            exceptionDetails += GetExceptionDetails(ex);
+        }
+
+        DataPackage dataPackage = new DataPackage();
+        dataPackage.SetText(exceptionDetails);
+        Clipboard.SetContent(dataPackage);
+    }
+
+    private static string GetExceptionDetails(Exception ex)
+    {
+        var innerExceptionData = ex.InnerException == null ? "" :
+            $"Inner Exception:\n{GetExceptionDetails(ex.InnerException)}";
+        string details = $@"Message: {ex.Message}
+StackTrace: {ex.StackTrace}
+{innerExceptionData}";
+        return details;
+    }
+
+    private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.SelectedItemContainer != null)
+        {
+            switch (args.SelectedItemContainer.Tag)
+            {
+                case "TextRecognizer":
+                    rootFrame.Navigate(typeof(TextRecognizerPage));
+                    break;
+
+                case "Translator":
+                    rootFrame.Navigate(typeof(TranslatorPage));
+                    break;
+            }
+        }
+    }
+}
