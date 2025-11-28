c20821b781fd1320e4c87ee16502dd79c20eb3aa
yinuoma112-dot <yinuoma112@gmail.com>
2025-11-28 10:46:36 +0800

    Add files via upload

---
+++ ViewModels/CopilotRootViewModel.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using WindowsAISample.Models.Contracts;
+
+namespace WindowsAISample.ViewModels;
+
+/// <summary>
+/// Our root model to expose all the AI Fabric API's
+/// </summary>
+internal class CopilotRootViewModel
+{
+    internal CopilotRootViewModel()
+    {
+        TextRecognizer = new(new Models.TextRecognizerModel());
+    }
+
+    public TextRecognizerViewModel TextRecognizer { get; }
+
+}
