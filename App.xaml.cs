80b9eaeeeb9b7f55d077418a5b2638223edd2976
yinuoma112-dot <yinuoma112@gmail.com>
2025-11-28 10:15:27 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using AIDevGallery.Sample.Utils;
+using Microsoft.UI.Xaml;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WindowsAISample
+{
+    public partial class App : Application
+    {
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Window = new MainWindow();
+            Window.Activate();
+            WindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(Window);
+        }
+        public static MainWindow? Window { get; private set; }
+        public static IntPtr WindowHandle { get; private set; }
+    }
+}
+
