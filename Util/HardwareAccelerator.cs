d5ffaf49dfda19a877dd783509a55d200aa6f27a
yinuoma112-dot <yinuoma112@gmail.com>
2025-11-28 10:44:40 +0800

    Add files via upload

---
+++ Util/HardwareAccelerator.cs
@@ -0,0 +1,15 @@
+namespace AIDevGallery.Sample.Utils;
+
+internal enum HardwareAccelerator
+{
+    CPU,
+    DML,
+    QNN,
+    WCRAPI,
+    OLLAMA,
+    OPENAI,
+    NPU,
+    GPU,
+    VitisAI,
+    OpenVINO,
+}
